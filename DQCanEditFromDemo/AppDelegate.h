69c2c8535af3649f75759bd7a931a13ac26711b4
琪琪邓 <15013781186@163.com>
2016-12-24 16:37:40 +0800

    Initial Commit

---
+++ DQCanEditFromDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DQCanEditFromDemo
+//
+//  Created by 邓琪 dengqi on 2016/12/24.
+//  Copyright © 2016年 邓琪 dengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DQCanEditFromDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DQCanEditFromDemo
+//
+//  Created by 邓琪 dengqi on 2016/12/24.
+//  Copyright © 2016年 邓琪 dengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
